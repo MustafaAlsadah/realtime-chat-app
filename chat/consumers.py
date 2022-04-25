66780bb79f128c526166cdc4a20bf4e666fed334
MustafaAlsadah <sadah9999@gmail.com>
2022-04-25 18:21:06 +0300

    front -end designing and start message handling

--- chat/consumers.py
+++ chat/consumers.py
@@ -5,7 +5,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name = 'chat_'+str(self.room_name)
 
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -15,17 +15,17 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type':'tester_message',
-                'tester':'hello, world!'
+                'type':'send_tester_message',
+                'message':'hello members of group: '+str(self.room_group_name)
             }
         )
 
         await self.accept()
-    
-    async def tester_message(self, event):
-        tester = event['tester']
-        await self.send(text_data=json.dumps({
-            'tester':tester
+        
+    async def send_tester_message(self, kwargs):
+        message = kwargs['message']
+        await self.send(json.dumps({
+            'msg':message
         }))
 
     async def disconnect(self, close_code):
--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,6 @@ from django.urls import re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/<room_name>/', consumers.ChatRoomConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatRoomConsumer.as_asgi()),
+    #room_name variable'll be created and passed to the consumer as a url kwarg
 ]
