bc30a1afc946ffc0b79d66dacb13eb2e402294d9
MustafaAlsadah <sadah9999@gmail.com>
2022-04-26 04:53:43 +0300

    f

--- core/asgi.py
+++ core/asgi.py
@@ -1,8 +1,9 @@
 import os
+import django
 
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
+django.setup()
 application = get_asgi_application()
--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,9 +127,7 @@ STATIC_URL = 'static/'
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
+        "CONFIG": {"hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")]},
     },
 }
 
