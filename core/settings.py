47aeff8d706e88fa9c7672372c99f51ccfb480f4
MustafaAlsadah <sadah9999@gmail.com>
2022-04-26 03:40:21 +0300

    fixing Procfile

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2!_!e&u6hctiw%xxf0-i4h#9$f#(%bigidyar=vxjjuybqc)c&'
+# SECRET_KEY = 'django-insecure-2!_!e&u6hctiw%xxf0-i4h#9$f#(%bigidyar=vxjjuybqc)c&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
