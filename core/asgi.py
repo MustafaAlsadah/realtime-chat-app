b03d34d811cbb7e0f82d76f8d8b0479738d95d44
MustafaAlsadah <sadah9999@gmail.com>
2022-04-27 04:13:42 +0300

    daphne setting up

--- core/asgi.py
+++ core/asgi.py
@@ -2,8 +2,8 @@ import os
 import django
 
 from channels.routing import ProtocolTypeRouter
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
-application = get_asgi_application()
+application = get_default_application()
