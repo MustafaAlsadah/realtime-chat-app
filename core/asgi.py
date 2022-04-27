59fa8ae68f1f48ce8dd94992b0d1610c253da1a9
MustafaAlsadah <sadah9999@gmail.com>
2022-04-27 04:47:41 +0300

    editing asgi.py

--- core/asgi.py
+++ core/asgi.py
@@ -1,8 +1,7 @@
 import os
 import django
 
-from channels.routing import ProtocolTypeRouter
-from django.core.asgi import get_default_application
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
